487de73b9349fd166abbf43b01d4959acb9d9116
WJYEE <wowoca7@gmail.com>
2025-05-27 01:58:55 +0900

    chores: remove token

--- auto-planner-backend/src/main.ts
+++ auto-planner-backend/src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // ✅ 테스트용 Notion 토큰 등록
-  saveToken('202255150', 'ntn_');  
+  saveToken('202255150', 'NOTION_API_KEY..');  
 
   // ✅ Render 호환을 위한 포트 설정
   await app.listen(process.env.PORT || 4523, '0.0.0.0');
