21961f00767f616bab7b7eb08e84552cfd20f7d3
suhyun <ksh114610@gmail.com>
2025-06-01 18:03:31 +0900

    fix: svelte-kit -> build

--- frontend/idh/svelte.config.js
+++ frontend/idh/svelte.config.js
@@ -2,11 +2,11 @@ import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		edge: false,      
-    	split: true  
-	}) }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()  // ← split, edge 생략 (기본은 정적 배포용)
+  }
 };
 
 export default config;
+
